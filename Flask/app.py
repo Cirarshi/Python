d9f778f9fbb87365b31ec78e02e8938a7f19b8af
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-08-23 01:41:52 +0530

    Flask basics

--- Flask/app.py
+++ Flask/app.py
@@ -12,5 +12,23 @@ def home():
 def about():
     return "This is a About for the Flask application."
 
+@app.route('/contact/<name>')
+
+def contact(name):
+    name = "Hello " + name + "!"
+    return name
+
+@app.route('/square/<int:number>')
+
+def square(number):
+    result = number * number
+    return f"The square of {number} is {result}."
+
+@app.route('/add/<a>/<b>')
+
+def add(a, b):
+    result = int(a) + int(b)
+    return f"The sum of {a} and {b} is {result}."
+
 if __name__ == '__main__':
     app.run(debug=True)
